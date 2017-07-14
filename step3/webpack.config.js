e7e86d6f85c88d8c268185a4a8ca52bdd649c08f
incuisting <xuyanting9634@163.com>
2017-07-14 16:52:45 +0800

    add page.html

---
+++ step3/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        }, ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    }
+}
