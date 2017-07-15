15e4578220b88e8caf9abb9a1424011d622b4e98
incuisting <xuyanting9634@163.com>
2017-07-15 21:47:03 +0800

    init step5

---
+++ step5/src/app.js
@@ -0,0 +1,97 @@
+import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'k6SOIiFIuxrQRmjsXD7sPbxq-gzGzoHsz'
+var APP_KEY = 'KHrbNCeqsmqKPctnhyYrzIzh'
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        actionType: 'signUp',
+        formData: {
+            username: '',
+            password: ''
+        },
+        newTodo: '',
+        todoList: [],
+        currentUser: null
+    },
+    created: function() {
+        window.onbeforeunload = () => { //当窗口即将被卸载时,会触发该事件
+            let dataString = JSON.stringify(this.todoList)
+            let newTodoValue = JSON.stringify(this.newTodo)
+            window.localStorage.setItem('myTodos', dataString) //存储输入款内容
+            window.localStorage.setItem('todoInputValue', newTodoValue)
+        }
+        let todoInputValue = window.localStorage.getItem('todoInputValue')
+        let todoInputData = JSON.parse(todoInputValue) //输入框的数据
+
+        let oldDataString = window.localStorage.getItem('myTodos') //取出数据
+        let oldData = JSON.parse(oldDataString) //转化成JSON格式
+
+        this.todoList = oldData || [] // 如果没有oldData则默认为空数组
+        this.newTodo = todoInputData || '' //为输入框添加上一次未提交的内容
+
+        this.currentUser = this.getCurrentUser()
+        console.log('current', this.currentUser)
+    },
+    methods: {
+        addTodo: function() {
+            this.todoList.push({ //this 被指向data
+                title: this.newTodo,
+                data: new Date(),
+                done: false
+            })
+            console.log(this.todoList)
+            this.newTodo = '' //变成空
+        },
+        removeTodo: function(todo) {
+            let index = this.todoList.indexOf(todo)
+            this.todoList.splice(index, 1)
+        },
+        //TODO
+        //在addTodo的时候就把时间的格式转化好
+        //尝试createAt直接指向一个methods
+        signUp: function() { //注册
+            let user = new AV.User();
+            user.setUsername(this.formData.username);
+            user.setPassword(this.formData.password);
+            user.signUp().then((loginedUser) => {
+                this.currentUser = this.getCurrentUser()
+            }, (error) => {
+                alert('注册失败')
+            });
+        },
+        login: function() { //登入
+            AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
+                this.currentUser = this.getCurrentUser()
+            }, (error) => {
+                alert('注册成功')
+            });
+        },
+        getCurrentUser: function() {
+            let current = AV.User.current()
+
+            if (current) {
+                let { id, createAt, attributes: { username } } = AV.User.current()
+                    //使用了ES6的解构赋值
+                return { id, username, createAt } //
+
+            } else {
+                return null
+            }
+        },
+        logout: function() {
+            AV.User.logOut()
+            this.currentUser = null
+            window.location.reload()
+        }
+
+    }
+
+})
